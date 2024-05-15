807a0521e8287a7e647de18236e85614618538c8
Sriteja <148988307+sriteja123gujjari@users.noreply.github.com>
2024-05-15 23:48:56 +0530

    Add files via upload
    This is a code from codechef

---
+++ katapa.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class katapa {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        int w=sc.nextInt();
+        if(w%2==0){
+            
+        }
+    }
+    
+}
